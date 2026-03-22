3f36ab028046ebbb75b3fdc397bcf03572cffe1e
Dr. Mohammed AL Jebori <35558952+ALJEBORI@users.noreply.github.com>
2026-03-22 22:31:17 +0000

    python ingest file

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -6,56 +6,57 @@
 
 import pandas as pd
 from sqlalchemy import create_engine
+from tqdm import tqdm
+import click
 
 # File parameters
+chunksize = 100000
 year = 2021
 month = 1
-chunksize = 100000
-# DB parameters
-pg_user="root"
-pg_pass="root"
-pg_host="localhost"
-pg_port=5432
-pg_db="ny_taxi"
-target_table = "yellow_taxi_data"
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
+    dtype = {
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+        "fare_amount": "float64",
+        "extra": "float64",
+        "mta_tax": "float64",
+        "tip_amount": "float64",
+        "tolls_amount": "float64",
+        "improvement_surcharge": "float64",
+        "total_amount": "float64",
+        "congestion_surcharge": "float64"
+    }
 
+    parse_dates = [
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime"
+    ]
 
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
-
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-# Read a sample of the data
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-df = pd.read_csv(
-    prefix + 'yellow_tripdata_{year}-{month:02d}.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates
-)
+    # Read a sample of the data
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    df = pd.read_csv(
+        prefix + 'yellow_tripdata_{year}-{month:02d}.csv.gz',
+        dtype=dtype,
+        parse_dates=parse_dates
+    )
 
 # nrows=100,
 # Display first rows
-df.head()
+    df.head()
 
 # Check data types
 #df.dtypes
@@ -68,57 +69,60 @@ df.head()
 
 
 
-engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-conn = engine.connect() 
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    conn = engine.connect() 
 
 
 
 
 
-print(pd.io.sql.get_schema(df, name=target_table, con=engine))
+    print(pd.io.sql.get_schema(df, name=target_table, con=engine))
 
 
 
 
 
-df.head(n=0).to_sql(name=target_table, con=engine, if_exists='replace')
+    df.head(n=0).to_sql(name=target_table, con=engine, if_exists='replace')
 
 
 
 
 
-df_iter = pd.read_csv(
-    prefix + 'yellow_tripdata_{year}-{month:02d}.csv.gz',
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=chunksize
-)
-
-first = True
-for df_chunk in df_iter:
-
-    if first:
-        # Create table schema (no data)
-        df_chunk.head(0).to_sql(
-            name=target_table,
-            con=engine,
-            if_exists="replace"
-        )
-        first = False
-        print("Table created")
-
-    # Insert chunk
-    df_chunk.to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="append"
+    df_iter = pd.read_csv(
+        prefix + 'yellow_tripdata_{year}-{month:02d}.csv.gz',
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
     )
 
-    print("Inserted:", len(df_chunk))
+    first = True
+    for df_chunk in tqdm(df_iter):
+
+        if first:
+            # Create table schema (no data)
+            df_chunk.head(0).to_sql(
+                name=target_table,
+                con=engine,
+                if_exists="replace"
+            )
+            first = False
+            print("Table created")
+
+        # Insert chunk
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append"
+        )
+
+        print("Inserted:", len(df_chunk))
 
 
 
 
-dff = pd.read_sql(f"SELECT * FROM {target_table}", engine)
-dff.head()
+#dff = pd.read_sql(f"SELECT * FROM {target_table}", engine)
+#dff.head()
+
+if __name__ == "__main__":
+    run()
